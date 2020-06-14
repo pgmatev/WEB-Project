2392fa47199f60c1cfcfbc2c0eed614a6bf18a56
pgmatev <petargabriel.matev@gmail.com>
2020-06-15 00:23:25 +0300

    changes to responsive menu/still unhappy

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -2,8 +2,22 @@ function myFunction() {
     var x = document.getElementById("headerRight");
     console.log("Hello");
     if (x.className === "header-right") {
-        x.className += "responsive";
+        x.className += "-responsive";
     } else {
         x.className = "header-right";
     }
+    var y = document.getElementById("header");
+    console.log("Hello");
+    if (y.className === "header") {
+        y.className += "-responsive";
+    } else {
+        y.className = "header";
+    }
+    // var z = document.getElementById("headerLeft");
+    // console.log("Hello");
+    // if (z.className === "header-left") {
+    //     z.className += "-responsive";
+    // } else {
+    //     z.className = "header-left";
+    // }
 }
